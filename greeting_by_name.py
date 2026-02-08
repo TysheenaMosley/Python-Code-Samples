ea7bb458d24c454217803f933c4bb9a524997194
TysheenaMosley <tmosley2@my.nl.edu>
2026-02-08 16:56:22 -0600

    Add files via upload

---
+++ greeting_by_name.py
@@ -0,0 +1,7 @@
+# Name: Tysheena Mosley
+# Date: February 2026
+# Program Description:
+# This program asks the user for their name and greets them.
+
+name = input("Please enter your name: ")
+print("Hello, " + name + "! Welcome!")
