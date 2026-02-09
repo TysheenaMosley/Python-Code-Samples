d621862ce3012fe489b71cf6531a2f7598821b93
TysheenaMosley <tmosley2@my.nl.edu>
2026-02-08 20:50:44 -0600

    Add files via upload

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,10 @@
+# Name: Tysheena Mosley
+# Date: February 2026
+# Program Description:
+# This program converts a temperature from Fahrenheit to Celsius.
+
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+print("The temperature in Celsius is:", celsius)
