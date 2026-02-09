1c1b36370e67fbe9bc6685cc7fe36687f4009d2d
TysheenaMosley <tmosley2@my.nl.edu>
2026-02-08 20:44:10 -0600

    Add files via upload
    Miles per gallon

---
+++ mpg_calculator.py
@@ -0,0 +1,11 @@
+# Name: Tysheena Mosley
+# Date: February 8,2026
+# Program Description:
+# This program calculates miles per gallon (MPG) based on user input.
+
+miles = float(input("Please enter the number of miles driven: "))
+gallons = float(input("Please enter the number of gallons used: "))
+
+mpg = miles / gallons
+
+print("Your car's MPG is:", mpg)
