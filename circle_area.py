63b4b74849d8597e32df647fb95535acc13a462d
TysheenaMosley <tmosley2@my.nl.edu>
2026-02-08 20:37:39 -0600

    Add files via upload

---
+++ circle_area.py
@@ -0,0 +1,11 @@
+# Name: Tysheena Mosley
+# Date: February 8, 2026
+# Program Description:
+# This program calculates the area of a circle based on the radius entered by the user.
+
+import math
+
+radius = float(input("Please enter the radius of the circle: "))
+area = math.pi * radius ** 2
+
+print("The area of the circle is:", area)
